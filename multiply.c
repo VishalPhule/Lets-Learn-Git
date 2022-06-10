5e73b3f46c49c3529b8212f0136eb8ed163fe507
Vishal Phule <Vishalphule2000@gmail.com>
2022-06-10 23:31:03 +0530

    added multiply.c file

---
+++ multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, Mul;
+
+    printf("\nEnter two no: ");
+    scanf("%d %d", &a, &b);
+
+    Mul = a * b;
+
+    printf("Mul : %d", Mul);
+   
+    return (0);
+}
