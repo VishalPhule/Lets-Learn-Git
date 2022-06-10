aaf5d2a7cc2ee0dd03e3e7b6e4d5340100730ac7
Vishal Phule <Vishalphule2000@gmail.com>
2022-06-10 23:47:35 +0530

    adding dev.c

---
+++ div.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, div;
+
+    printf("\nEnter two no: ");
+    scanf("%d %d", &a, &b);
+
+    div = a / b;
+
+    printf("Mul : %d", div);
+   
+    return (0);
+}
